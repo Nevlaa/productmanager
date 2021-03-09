a631d487d6f0596037ec86c3fe013173579c7cee
Scott horsburgh <shorsburgh19@gmail.com>
2021-03-09 13:58:44 -0700

    -

--- model/database.php
+++ model/database.php
@@ -2,11 +2,10 @@
 
 $username = getenv('USERNAME');
 $password = getenv('PASSWORD');
-//$options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
-
+echo($username);
+echo($password);
 try {
-    $db = new PDO('mysql:host=127.0.0.1;dbname=shorsrgh_guitar_shop;port=3306', $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db = new PDO('mysql:host=us-cdbr-east-03.cleardb.com;dbname=heroku_0c0b9ba07013cb3', $username, $password);
 } catch (PDOException $e) {
     $error_message = $e->getMessage();
     include 'errors/db_error_connect.php';
